2971282e4ce4506a50cf2d4b0fe4879876d56b35
NI <136798444+CipherNI@users.noreply.github.com>
2023-09-12 17:37:11 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,43 @@
 def initial_state():
-    return (0, 0, 0)
+    return (8, 0, 0)
 
 def is_goal(s):
-    return True
+    a, b, c = s
+    return a == 4 and b == 4
 
 def successors(s):
-    x, y, z = s
-    return []
+    a, b, c = s
+    # List to store successor states and step costs
+    successors = []
+
+    # Pour from a to b
+    pour = min(a, 5 - b)
+    if pour > 0:
+        successors.append(((a - pour, b + pour, c), pour))
+
+    # Pour from a to c
+    pour = min(a, 3 - c)
+    if pour > 0:
+        successors.append(((a - pour, b, c + pour), pour))
+
+    # Pour from b to a
+    pour = min(b, 8 - a)
+    if pour > 0:
+        successors.append(((a + pour, b - pour, c), pour))
+
+    # Pour from b to c
+    pour = min(b, 3 - c)
+    if pour > 0:
+        successors.append(((a, b - pour, c + pour), pour))
+
+    # Pour from c to a
+    pour = min(c, 8 - a)
+    if pour > 0:
+        successors.append(((a + pour, b, c - pour), pour))
+
+    # Pour from c to b
+    pour = min(c, 5 - b)
+    if pour > 0:
+        successors.append(((a, b + pour, c - pour), pour))
+
+    return successors
